dedfc0f46100d5e05453378864102c47552c6bf4
vincetroid <vince_trance@hotmail.com>
2019-06-21 17:56:17 -0500

    Animar desde un Atlas JSON un Atlas de un personaje corriendo

--- SpriteSheet.js
+++ SpriteSheet.js
@@ -4,21 +4,23 @@ var gSpriteSheets = {};
 var spriteSheet = {
     img: null,
     url: "",
-    sprites: new Array(),
+    sprites: [],
 
     init: function() { console.log('init')},
-    load: function(imgName) {
-        this.url = imgName;
 
+    load: function(imgName) {
+        
         var img = new Image();
         img.src = imgName;
-
         this.img = img;
-
+        this.url = imgName;
         gSpriteSheets[imgName] = this;
+        // console.log('gSpriteSheets');
+        // console.log(gSpriteSheets);
     },
 
     defSprite: function (name, x, y, w, h, cx, cy) {
+        // debugger
         var spt = {
             "id": name,
             "x": x,
@@ -32,7 +34,9 @@ var spriteSheet = {
     },
 
     parseAtlasDefinition: function(atlasJSON) {
+        
         // var parsed = JSON.parse(atlasJSON);
+        // debugger;
         var parsed = atlasJSON;
 
         for(var key in parsed.frames) {
@@ -47,8 +51,11 @@ var spriteSheet = {
                 cy = sprite.spriteSourceSize.y - (sprite.sourceSize.h * 0.5);
             }
 
+            // console.log('filename');
+            // console.log(sprite.filename);
             //Definir el sprite para esta hoja
-            this.defSprite(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy)
+            this.defSprite(sprite.filename, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy)
+            // this.defSprite(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy)
         }
     },
 
@@ -57,11 +64,14 @@ var spriteSheet = {
     // atlas, and find which one matches the name.
 	getStats: function (name) {
         // For each sprite in the 'sprites' Array...
+        // debugger
 		for(var i = 0; i < this.sprites.length; i++) {
             
             // Check if the sprite's 'id' parameter
             // equals the passed in name...
+            // debugger;
             if(this.sprites[i].id === name) {
+                debugger;
                 // and return that sprite if it does.
                 return this.sprites[i];
             }
@@ -70,13 +80,17 @@ var spriteSheet = {
 
         // If we don't find the sprite, return null.
 		return null;
-	}
+    },
+    
+
 }
 // });
 
 function drawSprite(spriteName, posX, posY){
     //Iterar por todos los spritesheets para encontrar el que queremos
+    // debugger;
     for (const sheetName in gSpriteSheets) {
+        // debugger;
         const sheet = gSpriteSheets[sheetName];
         const sprite = sheet.getStats(spriteName);
         if (sprite == null) continue;
@@ -89,13 +103,14 @@ function drawSprite(spriteName, posX, posY){
 
 function __drawSpriteInternal(spt, sheet, posX, posY) {
     if (spt == null || sheet == null) return;
-
+    
     var hlf = {
         x: spt.cx,
         y: spt.cy
     };
-
-    ctx.drawImage(sheet.img, spt.x, spt.y, spt.w, spt.h, posX + hlf.x, posY + hlf.y, spt.w, spt.h);
+    
+    // debugger;
+    context.drawImage(sheet.img, spt.x, spt.y, spt.w, spt.h, posX + hlf.x, posY + hlf.y, spt.w, spt.h);
 }
 
 function test() {
