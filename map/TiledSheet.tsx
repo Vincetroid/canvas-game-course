b4e7950005071a20d78f5f9bba8e0cd69586a7f8
vincetroid <vince_trance@hotmail.com>
2019-06-28 16:14:17 -0500

    Inicio de creacion de mapa con TILEDMapClass.js, imagen de sprite de zelda, assets de programa Tiled y su Json

---
+++ map/TiledSheet.tsx
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="TiledSheet" tilewidth="16" tileheight="16" tilecount="90" columns="10">
+ <image source="../zelda_sprites/16x16Sprites.png" width="160" height="144"/>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="4.8">
+    <polygon points="0,0 4.1,-3.8 11.9,-3.7 15.7,0 15.7,5.9 11,11 5,11 -0.1,6.1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="4.5">
+    <polygon points="0,0 4.1,-3.9 11.7,-4 15.8,0.1 15.8,7.3 13.2,11.3 2.8,11.4 0,7.4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="6" x="0.1" y="-0.1" width="15.9" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="-0.2" width="15.9" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="15.9" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.2" width="16" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.2" width="16.1" height="16.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.1">
+    <polygon points="0,0 11.9,0 11.7,-0.1 11.7,1.7 13,1.8 13,2.9 14,3 14,3.7 15.1,3.7 15.1,4.7 15.9,4.7 16,15.9 0.1,15.8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0.2" y="4.9">
+    <polygon points="0,0 4.8,-0.1 10.6,5.2 11.7,7.2 11.7,11.1 -0.3,11"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="-0.1">
+    <polygon points="0,0 15.7,0.1 15.7,16.1 -0.1,16.1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.1" y="-0.3">
+    <polygon points="0,0 2.2,0.2 4.1,2.1 6,0.2 10.2,0.2 13.8,3.3 15.1,6 15.9,6.3 15.8,10 14.1,12.1 14.1,13.3 14.9,13.2 15.1,14.2 16,14.2 16.1,16.1 0.2,16.1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="14" height="14.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="16.1" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="-0.1">
+    <polygon points="0,0 12.8,0.1 15.9,3 15.9,10.1 12.9,14.4 12.9,15.9 -0.1,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="16" height="16.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index">
+   <object id="1" x="0.4" y="-0.1">
+    <polygon points="0,0 2.5,5.7 4.4,8 4.5,11.9 6.7,14.1 11.6,14 12.6,14.8 14.5,15.9 15.4,16 15.6,-0.2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index">
+   <object id="1" x="15.6" y="-0.1">
+    <polygon points="0,0 -2.7,6.1 -4.5,8.1 -4.5,11.8 -6.7,14.1 -11.7,14 -13.6,15.9 -15.6,16 -15.6,0.1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="0">
+    <polygon points="0,0 15.6,0 15.8,1.9 13.9,3.6 13.9,4.7 14.8,4.7 15.7,6 15.9,9.7 14,11.7 15.9,13.8 15.9,15.7 0.1,15.8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="16" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="1" x="0.1" y="-0.2" width="16.1" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="16.2" height="16.2"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.1" width="16.1" height="16.3"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.1" height="16.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.1">
+    <polygon points="0,0 12.9,-0.1 13.1,3.8 11.9,5.9 11.1,6.9 8,8.3 8,10.8 6,12.8 4,13.8 -0.1,13.8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index">
+   <object id="1" x="1.2" y="3.9">
+    <polygon points="0,0 2.7,-2.8 10.7,-3 13.7,0.3 13.8,7.7 11,11 2.9,11 -0.1,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.1" y="0" width="16.2" height="16.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.1" y="0" width="16" height="14.9"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0.1" width="14.3" height="15.9"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index">
+   <object id="1" x="1.1" y="-0.1" width="14.8" height="16.1"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.1" y="0">
+    <polygon points="0,0 12.1,0.1 15,3.2 15,9.9 14.1,12 12,13.5 12,15.7 0.1,15.8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.1" y="-0.1" width="16.4" height="16.4"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index">
+   <object id="1" x="1.8" y="7.1">
+    <polygon points="0,0 4.2,-4.1 8.1,-4.2 12,-0.1 12.1,5.6 9.2,8.8 3.4,8.8 0,5.9"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index">
+   <object id="2" x="-0.1" y="0" width="16.1" height="15.9"/>
+  </objectgroup>
+ </tile>
+</tileset>
